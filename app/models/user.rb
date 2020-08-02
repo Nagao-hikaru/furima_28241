1298c1ed8f926c3ef567d6467381bd532018045f
Nagao-hikaru <koyanagi29sushi@gmail.com>
2020-08-02 12:57:41 +0900

    userモデルテストの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,7 @@ class User < ApplicationRecord
          PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
          KANA_REGEX = /\A[ァ-ン]+\z/
   validates :nickname, presence: true
-  validates :email, uniqueness: true
-  validates :password, format: {with: PASSWORD_REGEX, message: ' Include both letters and numbers'}
+  validates :password, format: {with: PASSWORD_REGEX, message: 'Include both letters and numbers'}
   validates :birthday, presence: true
   with_options presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters'} do
     validates :family_name
