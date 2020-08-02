c830668743fe957b29407693fd18c1eef3bcb81f
Nagao-hikaru <koyanagi29sushi@gmail.com>
2020-08-02 09:18:09 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+         KANA_REGEX = /\A[ァ-ン]+\z/
+  validates :nickname, presence: true
+  validates :email, uniqueness: true
+  validates :password, format: {with: PASSWORD_REGEX, message: ' Include both letters and numbers'}
+  validates :birthday, presence: true
+  with_options presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters'} do
+    validates :family_name
+    validates :last_name
+  end
+  validates :family_name_kana, presence: true, format: {with: KANA_REGEX, message: 'Full-width katakana characters'}
+  validates :last_name_kana, presence: true, format: {with: KANA_REGEX, message: 'Full-width katakana characters'}
 end
+
