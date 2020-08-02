2797c1fde59764786ba0e7c1409e7a4e262c495e
Nagao-hikaru <koyanagi29sushi@gmail.com>
2020-08-02 11:14:48 +0900

    userモデルテスト項目

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe '#create' do
+    before do
+      @user = FactoryBot.build(:user)
+    end
+  
+
+  describe 'ユーザー新規登録' do
+    context '新規登録がうまくいく時' do
+     
+    end
+
+    context '新規登録がうまくいかない時' do
+      
+    end
+  end
+end
+end
