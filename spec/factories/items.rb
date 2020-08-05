bd061e56a736c95122b5a4bd547109863056dd74
Nagao-hikaru <koyanagi29sushi@gmail.com>
2020-08-05 09:25:24 +0900

    ログインユーザーのみの出品画面遷移

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :item do
-    
+    name {Faker::Game.platform}
+    description { Faker::Lorem.sentence}
+    price {Faker::Number.number(digits: 5)}
+
+    # idはfakerのナンバーがあるからそれ使う。
   end
 end
