3f377e33c37235514215bd0f3752b408954a0b18
Nagao-hikaru <koyanagi29sushi@gmail.com>
2020-08-10 16:36:09 +0900

    user_addressテーブルの作成

--- db/migrate/20200810065718_create_user_addresses.rb
+++ db/migrate/20200810065718_create_user_addresses.rb
@@ -1,7 +1,13 @@
 class CreateUserAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :user_addresses do |t|
-
+      t.integer :prefecture_id, null: false
+      t.string :city, null: false
+      t.string :address, null: false
+      t.string :building
+      t.string :tel, null: false
+      t.string :postal_code, null: false
+      t.references :item, null: false
       t.timestamps
     end
   end
